571ce396fa755afb78effa35392b78a0347b4010
nada <nada.mesbah@usmba.ac.ma>
2023-01-17 11:57:35 +0100

    Initial commit

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppSignalR.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync() 
+        {
+            await AddMessageToChat("", "User Connected!");
+            await base.OnConnectedAsync();
+        }
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage",user, message);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,49 @@
+using ChatAppSignalR.Server.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(options =>
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new [] {"application/octet-stream"})
+);
+
+var app = builder.Build();
+
+//we add the middleware
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+
+//we add the endpoints to our chat
+//now we are able to connect to our chatHub from the client
+//with that the server side is done for now 
+
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToFile("index.html");
+
+app.Run();
