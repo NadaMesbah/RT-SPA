5ac9ec123331241432e5f18b01e74dd3e5a0c8ea
nada <nada.mesbah@usmba.ac.ma>
2023-01-17 12:37:30 +0100

    Upgrade

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -4,10 +4,20 @@ namespace ChatAppSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
         public override async Task OnConnectedAsync() 
         {
-            await AddMessageToChat("", "User Connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            //Context.ConnectionId is the Key
+            //username is the value
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} joined the chat!");
             await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception) 
+        { 
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left the chat!");
         }
         public async Task AddMessageToChat(string user, string message)
         {
